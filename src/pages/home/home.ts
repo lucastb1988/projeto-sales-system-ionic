95770a01134dce8d7a8d7cd2eb2b98a1e9e67f96
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-23 16:07:57 -0300

    Navegação

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,8 +10,19 @@ import { IonicPage } from 'ionic-angular/navigation/ionic-page';
 })
 export class HomePage {
 
+  //construtor já vem injetado com os atributos, classes informadas
+  //no Angular quando quiser injetar uma classe basta informar o parametro no construtor
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login() {
+    //push irá empilhar uma pagina em cima da outra
+    //empilhar a página de CategoriasPage, irá chamar esta página sem precisar importar neste componente
+    //quando realizar o login irá automaticamente para tela de Categorias
+
+    //somente ao logar entra na página de categorias, não empilha uma página na outra
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
