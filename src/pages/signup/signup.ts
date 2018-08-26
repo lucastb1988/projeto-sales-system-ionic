423cb55bb1ba9cf0908243dc1ca70350eb6b430f
Lucas Banin <lucas.t.banin@gmail.com>
2018-08-26 13:22:19 -0300

    Criando um formGroup para controlar o formulario

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '../../../node_modules/@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,7 +9,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  //FormGroup ajuda a controlar o formulario, fazer validações
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      //formBuilder responsavel por instansciar fromGroup (injetar)
+      this.formGroup = this.formBuilder.group({
+        //recupera nome vindo do formulario (ex: <ion-input formControlName="nome") e faz uma validação assim como no backend, '' inicializa como valor padrão
+        nome: ['Lucas', [Validators.required, Validators.minLength(5), Validators.maxLength(80)]], //está preenchendo o campo somente para testes
+        email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+        tipo : ['1', [Validators.required]],
+        cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha : ['123', [Validators.required]],
+        logradouro : ['Rua Via', [Validators.required]],
+        numero : ['25', [Validators.required]],
+        complemento : ['Apto 3', []],
+        bairro : ['Copacabana', []],
+        cep : ['10828333', [Validators.required]],
+        telefone1 : ['977261827', [Validators.required]],
+        telefone2 : ['', []],
+        telefone3 : ['', []],
+        estadoId : [null, [Validators.required]],
+        cidadeId : [null, [Validators.required]]    
+      });
   }
 
   signupUser() {
